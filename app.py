5218bff565134474e5e00c4f4bb9bdee6873abe6
Silvio Luiz Stanzani <silvio.stanzani@gmail.com>
2025-08-25 10:00:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+'''
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,3 +9,18 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+'''
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from FastAPI on Azure!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
